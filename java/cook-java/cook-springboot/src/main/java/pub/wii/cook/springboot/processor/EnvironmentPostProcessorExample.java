890e5953a74461c6c5920a326dc479a402cfce09
wii <zhenkai.sun@qq.com>
2020-12-11 18:03:29 +0800

    mod decode

--- java/cook-java/cook-springboot/src/main/java/pub/wii/cook/springboot/processor/EnvironmentPostProcessorExample.java
+++ java/cook-java/cook-springboot/src/main/java/pub/wii/cook/springboot/processor/EnvironmentPostProcessorExample.java
@@ -12,20 +12,24 @@ import java.util.HashMap;
 public class EnvironmentPostProcessorExample implements EnvironmentPostProcessor {
     private static final String PREFIX_SECRET = "secret:";
 
+    private String decode(String secret) {
+        return secret;
+    }
+
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         HashMap<String, Object> props = new HashMap<>();
         for (PropertySource<?> ps : environment.getPropertySources()) {
-            System.out.println("sp name: " + ps.getName());
             if (ps instanceof OriginTrackedMapPropertySource) {
                 OriginTrackedMapPropertySource source = (OriginTrackedMapPropertySource) ps;
                 for (String name : source.getPropertyNames()) {
                     Object value = source.getProperty(name);
-                    System.out.println(name + " ======= " + value);
                     if (value instanceof String) {
                         String str = (String) value;
                         if (str.startsWith(PREFIX_SECRET)) {
-                            props.put(name, str.substring(PREFIX_SECRET.length()));
+                            // decode, or something else
+                            String dec = decode(str.substring(PREFIX_SECRET.length()));
+                            props.put(name, dec);
                         }
                     }
                 }
