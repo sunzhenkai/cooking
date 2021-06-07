16dcbf6b3b5f83cf9941936bacdfaa0432e452b3
wii <zhenkai.sun@qq.com>
2021-06-07 21:13:23 +0800

    add io test

--- langs/python/cook-tornado/handler/benchmark_handler.py
+++ langs/python/cook-tornado/handler/benchmark_handler.py
@@ -10,28 +10,38 @@ from handler.base_handler import BaseHandler
 TIME_TO_SLEEP = .3
 
 
+def payload():
+    for i in range(10000 * 200):
+        pass
+
+
+async def async_payload():
+    payload()
+
+
 class TornadoCoroutineHandler(BaseHandler, ABC):
     @tornado.gen.coroutine
     def get(self):
-        yield time.sleep(TIME_TO_SLEEP)
+        yield payload()  # time.sleep(TIME_TO_SLEEP)
         self.write({'msg': 'ok'})
 
 
 class NativeCoroutineHandler(BaseHandler, ABC):
     async def get(self):
-        await asyncio.sleep(TIME_TO_SLEEP)
+        # await asyncio.sleep(TIME_TO_SLEEP)
+        await async_payload()
         self.write({'msg': 'ok'})
 
 
 class NativeCoroutineTaskHandler(BaseHandler, ABC):
     async def get(self):
-        await asyncio.create_task(asyncio.sleep(TIME_TO_SLEEP))
+        await asyncio.create_task(async_payload())
         self.write({'msg': 'ok'})
 
 
 class NativeCoroutineTaskTruncationHandler(BaseHandler, ABC):
     async def _do_get(self):
-        await asyncio.sleep(TIME_TO_SLEEP)
+        await async_payload()
 
     async def get(self):
         await asyncio.wait([self._do_get()], timeout=0.1)
@@ -40,5 +50,6 @@ class NativeCoroutineTaskTruncationHandler(BaseHandler, ABC):
 
 class NoCoroutineHandler(BaseHandler, ABC):
     def get(self):
-        time.sleep(TIME_TO_SLEEP)
+        # time.sleep(TIME_TO_SLEEP)
+        payload()
         self.write({'msg': 'ok'})
