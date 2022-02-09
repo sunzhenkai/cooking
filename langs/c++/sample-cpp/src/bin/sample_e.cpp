2735e3635c6cb4d1cc11d0c600dc164888038b6f
wii <zhenkai.sun@qq.com>
2022-02-09 21:26:16 +0800

    add sample

--- langs/c++/sample-cpp/src/bin/sample_e.cpp
+++ langs/c++/sample-cpp/src/bin/sample_e.cpp
@@ -2,31 +2,20 @@
 
 class A {
 public:
-    int v;
+  int v;
+};
 
-    A() = default;
-
-    A(const A &a) = default;
-
-    A(A &&a) = default;
-
-    void f() {
-        v = 1;
-    }
+class B {
+public:
+  A a;
+  A &b;
 };
 
 int main() {
-    A a{2};
-    A b = a;
-    std::cout << "b.v=" << b.v << std::endl;
-    auto f = [b = b]() mutable {
-        b.f();
-    };
-    f();
-    std::cout << "b.v=" << b.v << std::endl;
-
-    auto fa = [](A &&a) {
-        std::cout << a.v << std::endl;
-    };
-    return 0;
+  A a{1};
+  A &b = a;
+  std::string s = "abc";
+  s = "bcd";
+  std::cout << s << std::endl;
+  return 0;
 }
